303cc049e9902c64ee0b0dba20a61643b5aaeb97
Zayne <49252407+zaynedoc@users.noreply.github.com>
2025-05-14 07:10:02 +0000

    Initial commit

---
+++ helloWorld.java
@@ -0,0 +1,10 @@
+public class helloWorld {                       // File name must match public class name
+    public static void main(String[] args) {
+        System.out.println("Hello World!");     // println equivalent of newline ("\n")
+        System.out.print("Goodbye!\n");
+    }
+}
+
+// Recompiling will overwrite the previous executable
+// "javac helloWorld.java" > "java helloWorld.java"
+// Update javac to ver 24.0.1
